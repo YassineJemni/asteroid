73ec67c41262a44ea2fee231464daaae4cacded0
yassine jemni <yassinejemni6@gmail.com>
2025-02-07 14:15:06 +0100

    spawn asteroids

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ class Player{
         c.translate(this.position.x, this.position.y)
         c.rotate(this.rotation)
         c.translate(-this.position.x,-this.position.y)
+        c.beginPath();
         c.arc(this.position.x, this.position.y,5,0,Math.PI * 2,false);
         c.fillStyle = 'red'
         c.fill()
+        c.closePath();
         // c.fillStyle = 'red'
         // c.fillRect(this.position.x,this.position.y,100,100);
         c.beginPath();
@@ -63,6 +65,30 @@ class Projectile{
 
 }
 
+class Asteroid{
+    constructor({position,velocity,radius}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = 50*Math.random() +10
+        this.radius = radius
+    }
+
+    draw(){
+        c.beginPath();
+        c.arc(this.position.x,this.position.y,this.radius,0,Math.PI*2,false);
+        c.closePath();
+        c.strokeStyle = 'white';
+        c.stroke();
+    }
+
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+
+}
+
 
 const player = new Player({
     position:{x:canvas.width/2,y:canvas.height/2},
@@ -80,13 +106,57 @@ const FRICTION = 0.97
 const PROJECTILE_SPEED = 3
 
 const projectiles = []
+const asteroids = []
+
+window.setInterval(()=>{
+    const index = Math.floor(Math.random()*4)
+    let x,y
+    let vx,vy
+    let radius =  50*Math.random() + 10
+    switch (index) {
+        case 0:
+            x=0-radius//left side
+            y=Math.random()*canvas.height
+            vx=1
+            vy=0
+            break
+        case 1://bottom
+            x=Math.random() * canvas.width
+            y=canvas.height+radius
+            vx=0
+            vy=-1
+            break
+        case 2://right
+            x=canvas.width+radius
+            y=Math.random()*canvas.height
+            vx=-1
+            vy=0
+            break
+        case 3://top
+            x=Math.random() * canvas.width
+            y=0-radius
+            vx=0
+            vy=1
+            break
+    }
+
+    asteroids.push(new Asteroid({
+        position:{
+        x:x,
+        y:y},
+        velocity:{
+            x:vx,
+            y:vy},
+        radius,
+    }))
+},3000)
 
 function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black';
     c.fillRect(0,0,canvas.width,canvas.height);
     player.update()
-
+    //for projectiles
     for (let i=projectiles.length-1; i>=0; i--){
         const projectile = projectiles[i]
         projectile.update()
@@ -98,6 +168,19 @@ function animate(){
             projectiles.splice(i,1)
         }
     }
+    //for asteroids
+    for (let i=asteroids.length-1; i>=0; i--){
+        const asteroid = asteroids[i]
+        asteroid.update()
+        if(asteroid.position.x+asteroid.radius<0 || 
+            asteroid.position.x-asteroid.radius>canvas.width||
+            asteroid.position.y - asteroid.radius > canvas.height ||
+            asteroid.position.y + asteroid.radius <0
+        ){
+            asteroids.splice(i,1)
+        }
+    } 
+
 
     if(keys.w.pressed) {
         player.velocity.x = Math.cos(player.rotation) * SPEED
